0a310eebd9c7852fee16aee5c890ddba3e20bbb2
md-abutalha <abutalha03333@gmail.com>
2025-03-13 21:24:19 +0600

    api second

--- src/test/java/com/abu/talha/GETAPICall.java
+++ src/test/java/com/abu/talha/GETAPICall.java
@@ -6,19 +6,37 @@ import com.microsoft.playwright.APIRequest;
 import com.microsoft.playwright.APIRequestContext;
 import com.microsoft.playwright.APIResponse;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.RequestOptions;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class GETAPICall {
+    Playwright playwright;
+    APIRequest apiRequest;
+    APIRequestContext apiRequestContext;
 
-    @Test
-    public void getUserApiCall() throws IOException {  // Fixed method name
-        Playwright playwright = Playwright.create(); // Ensuring resources are closed
-        APIRequest apiRequest = playwright.request();
-        APIRequestContext apiRequestContext = apiRequest.newContext();
+    @BeforeSuite
+    public void beforeTest(){
+        playwright = Playwright.create();
+        apiRequest = playwright.request();
+        apiRequestContext = apiRequest.newContext();
+    }
+
+    @Test(priority = 1)
+    public void getSpecificAPITest(){
+        APIResponse apiResponse = apiRequestContext.get("https://gorest.co.in/public/v2/users",
+                RequestOptions.create()
+                        .setQueryParam("id","856168")
+                        .setQueryParam("status","inactive"));
+    }
+
+    @Test(priority = 2)
+    public void getUserApiCall() throws IOException {
         APIResponse apiResponse = apiRequestContext.get("https://gorest.co.in/public/v2/users");
 
         int status = apiResponse.status();
@@ -34,8 +52,16 @@ public class GETAPICall {
         System.out.println(jsonPrettyResponse);
 
         System.out.println(apiResponse.url());
+        Assert.assertEquals(apiResponse.url(),"https://gorest.co.in/public/v2/users");
+
 
         Map<String, String> headerMap = apiResponse.headers();
+        Assert.assertEquals(headerMap.get("content-type"),"application/json; charset=utf-8");
         System.out.println(headerMap);
+    }
+
+    @AfterSuite
+    public void tearDown(){
+        playwright.close();
     }
 }
